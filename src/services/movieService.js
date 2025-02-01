6f3a5845d0ccfc0cd2d42dc028ded648895f3a74
Joemer <joemers97@gmail.com>
2025-01-31 20:30:54 -0500

    adding authservices, movieservices

---
+++ src/services/movieService.js
@@ -0,0 +1,25 @@
+const BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER_URL}/movies/`;
+
+const index = async () => {
+	try {
+		const res = await fetch(BASE_URL, {
+			headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
+		});
+		return res.json();
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const show = async (movieId) => {
+	try {
+		const res = await fetch(`${BASE_URL}${movieId}/`, {
+			headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
+		});
+		return res.json();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export { index, show };
